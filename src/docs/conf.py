273a40fe387e88d9232669f85d2c29c7e587eb6f
Ita Santiago <50189748+sancas96@users.noreply.github.com>
2022-04-23 23:35:59 -0500

    cambiando version #8

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2022, Equipo2 Optimizacion2'
 author = 'Equipo2'
 
 # The short X.Y version
-version = ''
+version = '0.1.2'
 # The full version, including alpha/beta/rc tags
-release = '0.2'
+release = '0.1.2'
 
 
 # -- General configuration ---------------------------------------------------
